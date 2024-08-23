e90e45a2a04cfdfe0fef628b6a2ed849741165e5
Haoran Yin <hry@Haorans-MacBook-Pro.local>
2024-08-23 12:45:11 +0800

    Completed Lab 1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = sc.nextInt()  ;
+        System.out.println(age + "-year olds should date somebody who is at least " + (7 + age / 2) + " years old.");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = sc.nextInt()  ;
+        System.out.println(age + "-year olds should read at least " + (100 - age) + " pages before giving up on a book.");
     }
 }
